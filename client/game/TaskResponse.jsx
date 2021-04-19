0a18ea028f59f552aeb8ccdb8f1e7aa1e88b47f1
Jaeyoon Song <jyo3on@gmail.com>
2021-04-19 17:43:44 -0400

    taskresponse

--- client/game/TaskResponse.jsx
+++ client/game/TaskResponse.jsx
@@ -1,47 +1,79 @@
 import React from "react";
-import Slider from "meteor/empirica:slider";
+
+const questions = ["Name one of the female characters in the chosen story.", "Name one of the male characters in the chosen story.", "Is there any conversation in the chosen story?", "Is the ending of the chosen story happy or sad?"]
 
 export default class TaskResponse extends React.Component {
-  handleChange = num => {
+  state = { 
+    checked: false,
+    reasoned: false,
+    showSubmit: false,
+  };
+  question = questions[Math.floor(Math.random() * questions.length)]
+
+  handleChange = e => {
     const { player } = this.props;
-    const value = Math.round(num * 100) / 100;
-    player.round.set("value", value);
+
+    if (e.target.checked) {
+      player.round.set("value", e.target.id);
+      this.setState(prevState => ({
+        ...prevState,
+        checked: true,
+        showSubmit: prevState.reason
+      }));
+    }
   };
 
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.player.stage.submit();
+  handleReasonChange = e => {
+    const reason = e.target.value;
+    const { player } = this.props;
+
+    if (reason.trim().length > 0) {
+      player.round.set("reason", reason);
+      this.setState(prevState => ({
+        ...prevState,
+        reason: true,
+        showSubmit: prevState.checked
+      }));
+    }
+  };
+
+
+  handleSubmit = e => {
+    const { showSubmit } = this.state;
+    e.preventDefault();
+
+    if (showSubmit) {
+      this.props.player.stage.submit();
+    }
+    else {
+      alert("Please choose a story!")
+    }
   };
 
   renderSubmitted() {
     return (
       <div className="task-response">
         <div className="response-submitted">
-          <h5>Waiting on other players...</h5>
-          Please wait until all players are ready
+          <h5>You already submitted response...</h5>
+          If you are having trouble, please contact <a href="mailto:jyo3on@gmail.com">jyo3on@gmail.com</a>
         </div>
       </div>
     );
   }
 
-  renderSlider() {
-    const { player } = this.props;
-    const value = player.round.get("value");
+  renderCheckbox() {
     return (
-      <Slider
-        min={0}
-        max={1}
-        stepSize={0.01}
-        labelStepSize={0.25}
-        onChange={this.handleChange}
-        value={value}
-        hideHandleOnEmpty
-      />
+      <div className="checkbox">
+
+      </div>
     );
   }
 
   render() {
-    const { player } = this.props;
+    const { round, player } = this.props;
+    const { showSubmit } = this.state;
+    const stories = round.get("stories");
+    
 
     // If the player already submitted, don't show the slider or submit button
     if (player.stage.submitted) {
@@ -49,12 +81,52 @@ export default class TaskResponse extends React.Component {
     }
 
     return (
-      <div className="task-response">
-        <form onSubmit={this.handleSubmit}>
-          {this.renderSlider()}
+      <div>
+        <div className="task-stimulus">
+          <div className="full text-center">
+            <b>Please choose a better love story.</b>
+          </div>
+          <div className="full">
+            <div className="half story">
+              {stories[0].content}<br/>
+              {stories[0]._id}<br/>
+              {stories[0].allocated}
+            </div>
+            <div className="half story">
+              {stories[1].content}<br/>
+              {stories[1]._id}<br/>
+              {stories[0].allocated}
+            </div>
+          </div>
+        </div>
 
-          <button type="submit">Submit</button>
-        </form>
+        <div className="task-response">
+          
+          <form className="task-response-form" onSubmit={this.handleSubmit}>
+            <div className="full">
+              <div className="half">
+                <input id={"story" + stories[0]._id} name="story" className="btn" type="radio" onChange={this.handleChange}></input>
+                <label htmlFor={"story" + stories[0]._id}></label>
+              </div>
+              <div className="half">
+                <input id={"story" + stories[1]._id} name="story" className="btn" type="radio" onChange={this.handleChange}></input>
+                <label htmlFor={"story" + stories[1]._id}></label>
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="reason" className="text-center"><b>Why did you think so?</b></label>
+              <textarea id="reason" className="reason" onChange={this.handleReasonChange} placeholder="e.g., First, it has better word usage. Second, the plot is interesting and far from cliche. Lastly, the other one is just nonsensical."></textarea>
+            </div>
+
+            <div>
+              <label htmlFor="quiz" className="text-center"><b>{this.question}</b></label>
+              <textarea id="quiz" className="quiz" placeholder="Type answer to the question"></textarea>
+            </div>
+
+            <button type="submit" className={ showSubmit ? "submit" : "submit inactive" }>Submit</button>
+          </form>
+        </div>
       </div>
     );
   }
