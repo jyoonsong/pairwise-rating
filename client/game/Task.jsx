de417c2985f04b7a3f2bf9cb79126d5645ea3392
Jaeyoon Song <jyo3on@gmail.com>
2021-04-19 17:43:52 -0400

    round and task organized

--- client/game/Task.jsx
+++ client/game/Task.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
 import TaskResponse from "./TaskResponse";
-import TaskStimulus from "./TaskStimulus";
 
 export default class Task extends React.Component {
   render() {
     return (
       <div className="task">
-        <TaskStimulus {...this.props} />
         <TaskResponse {...this.props} />
       </div>
     );
