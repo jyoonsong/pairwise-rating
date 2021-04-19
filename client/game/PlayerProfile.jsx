49a62b98114efcdfd1c7ed953e6056e25832b859
Jaeyoon Song <jyo3on@gmail.com>
2021-04-19 17:43:38 -0400

    profile

--- client/game/PlayerProfile.jsx
+++ client/game/PlayerProfile.jsx
@@ -3,26 +3,37 @@ import React from "react";
 import Timer from "./Timer.jsx";
 
 export default class PlayerProfile extends React.Component {
+  state = { score: this.props.player.get("score") };
+
   renderProfile() {
     const { player } = this.props;
     return (
       <div className="profile-score">
-        <h3>Your Profile</h3>
-        <img src={player.get("avatar")} className="profile-avatar" />
+        <p><b>Your Worker ID</b>: {player.id}<br/><br/></p>
       </div>
     );
   }
 
   renderScore() {
     const { player } = this.props;
+    const score = player.get("score");
+
     return (
       <div className="profile-score">
-        <h4>Total score</h4>
-        <span>{(player.get("score") || 0).toFixed(2)}</span>
+        <b>Current status</b>:<br/>
+        <span>{score} rounds completed</span><br/><br/>
+
+        <b>Expected reward</b>:<br/>
+        {score < 10 ? 
+          <span>$0 <br/><small>*You need to complete at least 10 rounds</small></span>
+        :
+          <span>$2 ~ ${(score * 0.2).toFixed(1)}</span>
+        }
+        <br/>
       </div>
+
     );
   }
-
   render() {
     const { stage } = this.props;
 
