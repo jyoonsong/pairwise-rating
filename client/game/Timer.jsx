9c6503d44e4445a754288bdbb406a1c3b8ad6861
Jaeyoon Song <jyo3on@gmail.com>
2021-04-19 17:43:57 -0400

    timer modified

--- client/game/Timer.jsx
+++ client/game/Timer.jsx
@@ -3,19 +3,22 @@ import React from "react";
 
 class timer extends React.Component {
   render() {
-    const { remainingSeconds } = this.props;
+    const { remainingSeconds, stage } = this.props;
+
+    console.log(this.props)
 
     const classes = ["timer"];
-    if (remainingSeconds <= 5) {
+    if (stage.durationInSeconds - remainingSeconds <= 10) {
       classes.push("lessThan5");
-    } else if (remainingSeconds <= 10) {
+    } else if (stage.durationInSeconds - remainingSeconds <= 20) {
       classes.push("lessThan10");
     }
 
     return (
       <div className={classes.join(" ")}>
-        <h4>Timer</h4>
-        <span className="seconds">{remainingSeconds}</span>
+        <h4>Time spent</h4>
+        <span className="seconds">{stage.durationInSeconds - remainingSeconds}</span><br/>
+        <small>*You should spend at least 20 seconds in each stage.</small>
       </div>
     );
   }
